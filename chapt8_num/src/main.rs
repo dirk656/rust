b16f3623ca8060f74b60f3780f3d942741c23d0b
feng <dearronfeng0520@163.com>
2025-09-26 16:37:12 +0800

    add the way of enum

--- chapt8_num/src/main.rs
+++ chapt8_num/src/main.rs
@@ -16,11 +16,20 @@ fn print_enum(data:Color){
         Color::Blue => println!("blue"),
     }
 }
-
+fn print_building(data:BuildingLocation){
+    match data{
+        BuildingLocation::Number(num) => println!("number:{}",num),
+        BuildingLocation::Name(name) => println!("name:{}",name),
+    }
+}
 
 fn main ()
 {
     print_enum(Color::Red);
     print_enum(Color::Green);
     print_enum(Color::Blue);
+    let num = 1;
+    let name = "building A".to_string();
+    print_building(BuildingLocation::Name(name));
+    print_building(BuildingLocation::Number(num));
 }
