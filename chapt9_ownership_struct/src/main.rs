2bee4a390d942609c6ac56377d6cdf5cc0c58603
feng <1608749191@qq.com>
2025-09-27 08:57:03 +0800

    first commit

---
+++ chapt9_ownership_struct/src/main.rs
@@ -0,0 +1,23 @@
+struct Counter{
+    number:i32,
+}
+
+impl Counter{
+    fn new(number:i32)->Self{
+        Self{number}
+    }
+    // Bukebianjieyong
+    fn get_number(&self)->i32{
+        self.number
+    }
+
+    fn add(&mut self , increment:i32){
+        self.number + increment;
+    }
+}
+
+
+fn main ()
+{
+    
+}
